f7d331a8284ae88bf30d515c3ac278eb786aae86
USER <USER@20.0.78.127>
2026-07-09 14:44:15 -0500

    datos traslados

---
+++ src/Logica_de_salida/Herramientas_UML.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica_de_salida;
+
+/**
+ *
+ * @author USER
+ */
+public class Herramientas_UML {
+    
+}
